dd48ac657005b37a8b7bfedd5f44259f1fd2def3
ExequielSilvestre <exequiel.silvestre@scalar.io>
2024-03-24 19:11:29 -0300

    Class based views

--- backend/tutorial/projects/urls.py
+++ backend/tutorial/projects/urls.py
@@ -3,8 +3,8 @@ from projects import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('projects/', views.project_list),
-    path('projects/<int:pk>/', views.project_detail),
+    path('projects/', views.ProjectList.as_view()),
+    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
