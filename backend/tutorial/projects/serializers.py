ef8e501c8c0e2ea5c9daa6e0c72bd16d1f1b1b5f
ExequielSilvestre <exequiel.silvestre@scalar.io>
2024-03-24 20:19:27 -0300

    Authentication and permissions

--- backend/tutorial/projects/serializers.py
+++ backend/tutorial/projects/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from projects.models import Projects, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+from rest_framework import permissions
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
-        fields = ['id', 'name', 'active', 'finish_date']
+        fields = ['id', 'name', 'active', 'finish_date', 'owner', 'permissions_classes']
         read_only_fields = ['id']
         extra_kwargs = {
             'active': {'default': True},
         }
+        owner = serializers.ReadOnlyField(source='owner.username')
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Projects.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets', 'permissions_classes']
--- backend/tutorial/projects/views.py
+++ backend/tutorial/projects/views.py
@@ -1,13 +1,29 @@
 from projects.models import Projects
-from projects.serializers import ProjectSerializer
+from projects.serializers import ProjectSerializer, UserSerializer
 from rest_framework import generics
-
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from projects.permissions import IsOwnerOrReadOnly
 
 class ProjectList(generics.ListCreateAPIView):
     queryset = Projects.objects.all()
     serializer_class = ProjectSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
 
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Projects.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
